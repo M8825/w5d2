30def05378756d05001b7f1af129bfdbd020a6e1
Mlkz <maho.mamulashvili@gmail.com>
2022-12-07 09:27:29 -0500

    Working on sparse continent

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -121,5 +121,18 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT 
+      name, continent, population
+    FROM
+      countries
+    WHERE continent IN
+  (SELECT 
+      continent
+    FROM  
+      countries
+    WHERE 
+      population < 25000000
+    GROUP BY
+      continent)
   SQL
 end
